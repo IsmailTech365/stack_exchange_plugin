9f08cf7c2cd7e67aa5349e662cc2c5b97f9ace19
IsmailTech365 <ismailtech365@gmail.com>
2024-08-23 16:25:43 +0300

    h

--- android/src/main/kotlin/com/example/stack_exchange_plugin/StackExchangePlugin.kt
+++ android/src/main/kotlin/com/example/stack_exchange_plugin/StackExchangePlugin.kt
@@ -1,72 +1,35 @@
 package com.example.stack_exchange_plugin
 
-import android.content.Context
-import android.os.AsyncTask
-import android.widget.Toast
+import androidx.annotation.NonNull
+
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
 
-class StackExchangePlugin: FlutterPlugin, MethodChannel.MethodCallHandler {
-    private lateinit var channel: MethodChannel
-    private lateinit var context: Context
+/** StackExchangePlugin */
+class StackExchangePlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
 
-    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(binding.binaryMessenger, "stack_exchange_plugin")
-        context = binding.applicationContext
-        channel.setMethodCallHandler(this)
-    }
-override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
-    if (call.method == "fetchData") {
-        fetchData(result)
+  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "stack_exchange_plugin")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
-        result.notImplemented()
+      result.notImplemented()
     }
-}
+  }
 
-private fun fetchData(result: MethodChannel.Result) {
-    FetchDataTask(context, result).execute()
-}
-
-private class FetchDataTask(
-    private val context: Context,
-    private val result: MethodChannel.Result
-) : AsyncTask<Void, Void, String>() {
-
-    override fun doInBackground(vararg params: Void?): String {
-        return try {
-            val url = URL("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
-            val inStream = BufferedReader(InputStreamReader(connection.inputStream))
-            val content = StringBuilder()
-            var inputLine: String?
-            while (inStream.readLine().also { inputLine = it } != null) {
-                content.append(inputLine)
-            }
-            inStream.close()
-            connection.disconnect()
-            content.toString()  // Return the fetched data
-        } catch (e: Exception) {
-            "Failed to fetch data!"
-        }
-    }
-
-    override fun onPostExecute(resultString: String) {
-        // Show a toast with the result
-        Toast.makeText(context, resultString, Toast.LENGTH_LONG).show()
-        
-        // Send the result back to Flutter
-        result.success(resultString)
-    }
-}
-
-   
-
-
-
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 }
--- example/android/app/src/main/kotlin/com/example/stack_exchange_plugin_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/stack_exchange_plugin_example/MainActivity.kt
@@ -3,3 +3,77 @@ package com.example.stack_exchange_plugin_example
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity()
+import android.os.Bundle
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import retrofit2.*
+import retrofit2.converter.gson.GsonConverterFactory
+
+// Data classes to map the API response
+data class Question(
+    val title: String,
+    val link: String,
+    val score: Int
+)
+
+data class StackExchangeResponse(
+    val items: List<Question>
+)
+
+// Retrofit interface
+interface StackExchangeApiService {
+    @GET("questions?order=desc&sort=activity&site=stackoverflow")
+    fun getQuestions(
+        @Query("tagged") tags: String,
+        @Query("pagesize") pageSize: Int
+    ): Call<StackExchangeResponse>
+}
+
+// Main activity class implementing FlutterPlugin and MethodCallHandler
+class MainActivity : FlutterPlugin, MethodChannel.MethodCallHandler {
+    private lateinit var channel: MethodChannel
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "stack_exchange_plugin")
+        channel.setMethodCallHandler(this)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        if (call.method == "getQuestions") {
+            val tags = call.argument<String>("tags") ?: ""
+            val pageSize = call.argument<Int>("pageSize") ?: 10
+
+            val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.stackexchange.com/2.3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+            val service = retrofit.create(StackExchangeApiService::class.java)
+            val callApi = service.getQuestions(tags, pageSize)
+
+            callApi.enqueue(object : Callback<StackExchangeResponse> {
+                override fun onResponse(call: Call<StackExchangeResponse>, response: Response<StackExchangeResponse>) {
+                    if (response.isSuccessful) {
+                        val questions = response.body()?.items?.map {
+                            mapOf("title" to it.title, "link" to it.link, "score" to it.score)
+                        }
+                        result.success(questions)
+                    } else {
+                        result.error("API_ERROR", "Failed to fetch data", null)
+                    }
+                }
+
+                override fun onFailure(call: Call<StackExchangeResponse>, t: Throwable) {
+                    result.error("NETWORK_ERROR", t.message, null)
+                }
+            })
+        } else {
+            result.notImplemented()
+        }
+    }
+}
