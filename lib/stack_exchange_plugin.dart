a55ff45463cfe9d868fc6549a37e81348872eff0
IsmailTech365 <ismailtech365@gmail.com>
2024-08-23 16:56:24 +0300

    h2

--- lib/stack_exchange_plugin.dart
+++ lib/stack_exchange_plugin.dart
@@ -1,8 +1,15 @@
-
-import 'stack_exchange_plugin_platform_interface.dart';
+import 'package:flutter/services.dart';
 
 class StackExchangePlugin {
-  Future<String?> getPlatformVersion() {
-    return StackExchangePluginPlatform.instance.getPlatformVersion();
+  static const MethodChannel _channel = MethodChannel('stack_exchange_plugin');
+
+  static Future<List<Map<String, dynamic>>> getQuestions(
+      String tags, int pageSize) async {
+    final List<dynamic> questions =
+        await _channel.invokeMethod('getQuestions', {
+      'tags': tags,
+      'pageSize': pageSize,
+    });
+    return List<Map<String, dynamic>>.from(questions);
   }
 }
